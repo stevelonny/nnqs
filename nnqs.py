00ec7ac8d4a0bfe961c03e53eda665cd27dcdebb
Stefano Lonardoni <steve.lonny@outlook.com>
2025-07-09 10:53:13 +0200

    sampler wit TFP

--- nnqs.py
+++ nnqs.py
@@ -1,28 +1,47 @@
 import tensorflow as tf
 import numpy as np
 
+
 class RBM(tf.Module):
 
-    def __init__(self, n_visible, n_hidden):
+    def __init__(
+        self, n_visible, n_hidden, std_visible=0.1, std_hidden=0.1, std_weights=0.1
+    ):
         super(RBM, self).__init__()
 
         self.n_visible = n_visible
         self.n_hidden = n_hidden
 
-        self.a = tf.Variable(tf.random.normal(n_visible, stddev=0.01), name = 'visible_bias')
-        self.b = tf.Variable(tf.random.normal(n_hidden, stddev=0.01), name = 'hidden_bias')
-        self.W = tf.Variable(tf.random.normal((n_visible, n_hidden), stddev=0.01), name = 'weights')
+        self.std_visible = std_visible
+        self.std_hidden = std_hidden
+        self.std_weights = std_weights
+
+        self.a = tf.Variable(
+            tf.random.normal((n_visible,), stddev=std_visible), name="visible_bias"
+        )
+        self.b = tf.Variable(
+            tf.random.normal((n_hidden,), stddev=std_hidden), name="hidden_bias"
+        )
+        self.W = tf.Variable(
+            tf.random.normal((n_visible, n_hidden), stddev=std_visible), name="weights"
+        )
 
     def log_psi(self, samples):
-
-        sum_visible = tf.reduce_sum(self.a * samples, axis=1)
-        sum_hidden = tf.reduce_sum(tf.math.log(2.0*(tf.math.cosh(self.b + tf.matmul(samples, self.W)))), axis=1)
+        casted_samples = 2.0 * tf.cast(samples, tf.float32) - tf.ones_like(
+            samples, dtype=tf.float32
+        )
+        sum_visible = tf.reduce_sum(self.a * casted_samples, axis=1)
+        w_h = self.b + tf.matmul(casted_samples, self.W)
+        sum_hidden = tf.reduce_sum(tf.math.softplus(w_h), axis=1)
+        # sum_hidden = tf.reduce_sum(tf.math.log(2.0 * (tf.math.cosh(self.b + tf.matmul(casted_samples, self.W)))), axis=1)
 
         return sum_visible + sum_hidden
-    
+
     def psi(self, samples):
         return tf.exp(self.log_psi(samples))
 
-    def probability(self, samples):
-        return tf.exp(2.0*tf.math.real(self.log_psi(samples)))
+    def log_prob(self, samples):
+        return 2.0 * tf.math.real(self.log_psi(samples))
 
+    def probability(self, samples):
+        return tf.exp(self.log_prob(samples))
--- sampler.py
+++ sampler.py
@@ -1,12 +1,142 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
 import numpy as np
 
+
 class MRT2:
+    """
+    Metropolis-Hastings sampler for RBM wave functions using TensorFlow Probability.
+
+    This class implements a single-site Metropolis algorithm for binary spin systems.
+    It supports parallel chains, burn-in, and diagnostics for acceptance rate.
+
+    Attributes:
+        n_sites (int): Number of sites (spins) in the system.
+        batch_size (int): Number of samples to generate in each batch.
+        n_chains (int): Number of parallel Markov chains to run.
+        n_sweeps (int): Number of sweeps (MCMC steps) before returning samples.
+        current_state (tf.Variable): Current state of all chains.
+    """
+
     def __init__(self, n_sites, batch_size, n_chains, n_sweeps):
+        """
+        :param n_sites: number of sites in the system
+        :param batch_size: number of samples to generate in each batch
+        :param n_chains: number of parallel chains to run
+        :param n_sweeps: number of sweeps to perform before returning samples
+        """
         self.n_sites = n_sites
         self.batch_size = batch_size
         self.n_chains = n_chains
         self.n_sweeps = n_sweeps
 
+        self.current_state = tf.Variable(
+            tf.random.uniform((n_chains, n_sites), minval=0, maxval=2, dtype=tf.int32),
+            trainable=False,
+            name="mrt2_state",
+        )
+
+    def _single_site_flip(self, state, seed=None):
+        """
+        Propose a new state by flipping a single random site.
+        Compatible with TFP's new_state_fn signature.
+        """
+        batch_size = tf.shape(state)[0]
+        n_sites = self.n_sites
+
+        sites = tf.random.uniform((batch_size,), maxval=n_sites, dtype=tf.int32)
+
+        mask = tf.one_hot(sites, n_sites, dtype=tf.int32)
+
+        flipped_state = state + mask * (
+            tf.ones(tf.shape(state), dtype=tf.int32) - 2 * state
+        )
+        return flipped_state
+
     def sample(self, wave_function):
-        pass
+        """
+        Generate samples using TensorFlow Probability MCMC.
+
+        :param wave_function: RBM wave function with log_prob method
+        :return: samples of shape (batch_size, n_sites)
+        """
+
+        def target_log_prob_fn(*state_parts):
+            if len(state_parts) == 1:
+                state = state_parts[0]
+            else:
+                state = state_parts[0]
+
+            return wave_function.log_prob(state)
+
+        kernel = tfp.mcmc.RandomWalkMetropolis(
+            target_log_prob_fn=target_log_prob_fn, new_state_fn=self._single_site_flip
+        )
+
+        samples_per_chain = tf.maximum(1, self.batch_size // self.n_chains)
+        # print(f"Samples per chain: {samples_per_chain.numpy()}")
+        samples = tfp.mcmc.sample_chain(
+            num_results=samples_per_chain,
+            current_state=self.current_state,
+            kernel=kernel,
+            num_burnin_steps=self.n_sweeps,  # Burnin steps
+            num_steps_between_results=1,
+            trace_fn=None,
+            parallel_iterations=self.n_chains,
+        )
+        self.current_state.assign(samples[-1])
+
+        samples_flat = tf.reshape(samples, (-1, self.n_sites))
+        final_samples = samples_flat[: self.batch_size]
+
+        return tf.cast(final_samples, tf.float32)
+
+    def get_acceptance_rate(self, kernel_results):
+        """
+        Calculate the acceptance rate from kernel results.
+
+        :param kernel_results: Results from MCMC sampling
+        :return: Average acceptance rate
+        """
+        return tf.reduce_mean(tf.cast(kernel_results.is_accepted, tf.float32))
+
+    def sample_with_diagnostics(self, wave_function):
+        """
+        Generate samples and return diagnostics.
+
+        :param wave_function: RBM wave function with log_prob method
+        :return: (samples, acceptance_rate)
+        """
+
+        def target_log_prob_fn(*state_parts):
+            if len(state_parts) == 1:
+                state = state_parts[0]
+            else:
+                state = state_parts[0]
+
+            return wave_function.log_prob(state)
+
+        kernel = tfp.mcmc.RandomWalkMetropolis(
+            target_log_prob_fn=target_log_prob_fn, new_state_fn=self._single_site_flip
+        )
+
+        samples_per_chain = tf.maximum(1, self.batch_size // self.n_chains)
+
+        samples, kernel_results = tfp.mcmc.sample_chain(
+            num_results=samples_per_chain,
+            current_state=self.current_state,
+            kernel=kernel,
+            num_burnin_steps=self.n_sweeps,
+            num_steps_between_results=1,
+            trace_fn=lambda _, results: results.is_accepted,
+            parallel_iterations=self.n_chains,
+        )
+
+        self.current_state.assign(samples[-1])
+
+        acceptance_rate = tf.reduce_mean(tf.cast(kernel_results, tf.float32))
+
+        samples_flat = tf.reshape(samples, (-1, self.n_sites))
+        final_samples = samples_flat[: self.batch_size]
+
+        return tf.cast(final_samples, tf.float32), acceptance_rate, kernel_results
