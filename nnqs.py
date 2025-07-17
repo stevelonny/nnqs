a5dca97bb68381aae85368f252a4151ceeffb3f7
Stefano Lonardoni <steve.lonny@outlook.com>
2025-07-17 20:17:04 +0200

    add docstrings to nnqs

--- nnqs.py
+++ nnqs.py
@@ -3,10 +3,29 @@ import numpy as np
 
 
 class RBM(tf.Module):
+    """Restricted Boltzmann Machine for quantum wave function representation.
+    
+    A complex-valued RBM that can be used as a variational ansatz for quantum
+    many-body systems. The wave function is parameterized as:
+    
+    ψ(σ) = exp(∑ᵢ aᵢσᵢ + ∑ⱼ log(2cosh(bⱼ + ∑ᵢ Wᵢⱼσᵢ)))
+    
+    where σᵢ ∈ {0,1} are visible units (spins), and all parameters are complex-valued.
+    """
 
     def __init__(
         self, n_visible=4, n_hidden=8, std_visible=0.1, std_hidden=0.1, std_weights=0.1, path=None
     ):
+        """Initialize the RBM with complex-valued parameters.
+        
+        Args:
+            n_visible: Number of visible units (spins/qubits).
+            n_hidden: Number of hidden units.
+            std_visible: Standard deviation for visible bias initialization.
+            std_hidden: Standard deviation for hidden bias initialization.
+            std_weights: Standard deviation for weight matrix initialization.
+            path: Optional path to load pre-trained parameters from .npz file.
+        """
         super(RBM, self).__init__()
         if path is not None:
             param = np.load(path)
@@ -47,6 +66,15 @@ class RBM(tf.Module):
 
     @tf.function
     def log_psi(self, samples):
+        """Compute the complex log-amplitude of the wave function.
+        
+        Args:
+            samples: Tensor of shape (batch_size, n_visible) containing binary 
+                    spin configurations (0s and 1s).
+        
+        Returns:
+            Complex tensor of shape (batch_size,) containing log ψ(σ) for each sample.
+        """
         casted_samples = 2.0 * tf.cast(samples, tf.complex64) - tf.ones_like(samples, dtype=tf.complex64)
         sum_visible = tf.reduce_sum(self.a * casted_samples, axis=1)
         w_h = self.b + tf.matmul(casted_samples, self.W)
@@ -56,10 +84,24 @@ class RBM(tf.Module):
         return sum_visible + sum_hidden
 
     def log_prob(self, samples):
+        """Compute log-probability for sampling from |ψ|².
+        
+        Args:
+            samples: Tensor of shape (batch_size, n_visible) containing binary 
+                    spin configurations.
+        
+        Returns:
+            Real tensor of shape (batch_size,) containing 2×Re[log ψ(σ)] = log|ψ(σ)|².
+        """
         logpsi = self.log_psi(samples)
         return 2.0 * tf.math.real(logpsi)
     
     def save_parameters(self, path):
+        """Save RBM parameters to a .npz file.
+        
+        Args:
+            path: String path where to save the parameters.
+        """
         np.savez(
             path,
             n_visible=self.n_visible,
@@ -70,6 +112,11 @@ class RBM(tf.Module):
         )
     
     def load_parameters(self, path):
+        """Load RBM parameters from a .npz file.
+        
+        Args:
+            path: String path to the .npz file containing saved parameters.
+        """
         data = np.load(path)
         self.n_visible = data['n_visible']
         self.n_hidden = data['n_hidden']
